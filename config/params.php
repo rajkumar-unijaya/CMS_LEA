1ff3265721c3a1639967813d4d21d09e4c9d04eb
DESKTOP-259JC68\Admin <rajkumar.ganni@gmail.com>
2021-02-22 20:43:16 +0800

    change params configuations values for to cms communicate to crud-api through DF.

--- config/params.php
+++ config/params.php
@@ -4,7 +4,9 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
-    "DreamFactoryContextURL" => 'http://127.0.0.1:8000/api/',
+    #"DreamFactoryContextURL" => 'http://127.0.0.1:8000/api/',
+    "DreamFactoryContextURL" => 'http://10.5.5.27:37595/api/v2/crud-api/records/',
     "DreamFactoryHeaderKey" => 'X-DreamFactory-API-Key',
-    "DreamFactoryHeaderPass" => '323326d1fc16ee93ee80df98d023e2caa73aa2b1911bd6ea9b83b311d09982f2',
+    #"DreamFactoryHeaderPass" => '323326d1fc16ee93ee80df98d023e2caa73aa2b1911bd6ea9b83b311d09982f2',
+    "DreamFactoryHeaderPass" => '36fda24fe5588fa4285ac6c6c2fdfbdb6b6bc9834699774c9bf777f706d05a88',
 ];
