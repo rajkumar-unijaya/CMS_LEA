eac62f3eaa6826e0e4c2fff9f81e1c5049353fda
DESKTOP-259JC68\Admin <rajkumar.ganni@gmail.com>
2021-03-04 11:54:08 +0800

    Change DF config values at params.php file for calling procedures and tables and changed the routings based on the Pretty URLs enables.

--- config/params.php
+++ config/params.php
@@ -4,14 +4,12 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
-    #"DreamFactoryContextURLProcedures" => 'http://127.0.0.1:8000/api/v2/',
-    "DreamFactoryContextURLProcedures" => 'http://10.5.5.27:37595/api/v2/',
-    #"DreamFactoryContextURL" => 'http://127.0.0.1:8000/api/v2/crud-api/records/', 
-    "DreamFactoryContextURL" => 'http://10.5.5.27:37595/api/v2/crud-api/records/', 
+    #"DreamFactoryContextURLProcedures" => 'http://127.0.0.1:8000/api/v2/', local
+    "DreamFactoryContextURLProcedures" => 'http://10.5.5.27:8081/api/v2/crud-api/procedure/',
+    #"DreamFactoryContextURL" => 'http://127.0.0.1:8000/api/v2/crud-api/records/', local
+    "DreamFactoryContextURL" => 'http://10.5.5.27:8081/api/v2/crud-api/table/records/', 
     "DreamFactoryHeaderKey" => 'X-DreamFactory-API-Key',
-    "DreamFactoryHeaderPass" => '7c174ae2f835c0b7fd9e63f65358cdb6958cd4b9a705d652e220b30f007fdb14',  
-    #"DreamFactoryHeaderPass" => '36fda24fe5588fa4285ac6c6c2fdfbdb6b6bc9834699774c9bf777f706d05a88', 
+    "DreamFactoryHeaderPass" => '7c174ae2f835c0b7fd9e63f65358cdb6958cd4b9a705d652e220b30f007fdb14',
     #"DreamFactoryHeaderPass" => '323326d1fc16ee93ee80df98d023e2caa73aa2b1911bd6ea9b83b311d09982f2', local
-    #"DreamFactoryHeaderKey" => 'X-DreamFactory-Session-Token',
-    #"DreamFactoryHeaderPass" => 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI3NjA3YWI4MDczMzU0MDg0NGRhMGU5MDQ3ZjRkNDEwMCIsImlzcyI6Imh0dHA6Ly8xMC41LjUuMjc6Mzc1OTUvYXBpL3YyL3N5c3RlbS9hZG1pbi9zZXNzaW9uIiwiaWF0IjoxNjE0MzAzNDI1LCJleHAiOjE2MTQzODk4MjUsIm5iZiI6MTYxNDMwMzQyNSwianRpIjoiU0c0cGk0cURjMHl6SmpxOCIsInVzZXJfaWQiOjgsImZvcmV2ZXIiOmZhbHNlfQ.SGq1zSNdv5deXbqEQh7RNS4ifuZA3K96Z-cqpgxfzAM',
+    
 ];
